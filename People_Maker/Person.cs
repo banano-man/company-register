3877982802ee7a0e22ae9daa455a2fb57abec5a5
Alex Dimitrov <78638967+AlexD174@users.noreply.github.com>
2022-03-18 18:01:28 +0200

    Update Person.cs

--- People_Maker/Person.cs
+++ People_Maker/Person.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -8,38 +8,43 @@ namespace People_Maker
 {
     public class Person
     {
+        private int index = 0;
         public Person(string First_Name, string Last_Name, char gender)
         {
+
             string conString = "Data Source = (LocalDb)\\MSSQLLocalDB; Initial Catalog = Firm_Register; Integrated Security=True";
             SqlConnection Con = new SqlConnection(@conString);
-            SqlCommand Cmd = new SqlCommand("INSERT INTO People " +
-            "(Person_Email, Person_Password, Person_First_Name, Person_Last_Name, Person_EGN) " +
-                    "VALUES(@Person_Email, @Person_Password, @Person_First_Name, @Person_Last_Name, @Person_EGN)", Con);
-            Cmd.Parameters.Add("@Person_Email", System.Data.SqlDbType.NVarChar);
-                Cmd.Parameters.Add("@Person_Password", System.Data.SqlDbType.NVarChar);
-                Cmd.Parameters.Add("@Person_First_Name", System.Data.SqlDbType.NVarChar);
-                Cmd.Parameters.Add("@Person_Last_Name", System.Data.SqlDbType.NVarChar);
-            Cmd.Parameters.Add("@Person_EGN", System.Data.SqlDbType.NVarChar);
+            SqlCommand CmdId = new SqlCommand("SELECT MAX(Person_Id) FROM People", Con);
+             SqlCommand Cmd = new SqlCommand("INSERT INTO People " +
+             "(Person_Email, Person_Password, Person_First_Name, Person_Last_Name, Person_EGN) " +
+                     "VALUES(@Person_Email, @Person_Password, @Person_First_Name, @Person_Last_Name, @Person_EGN)", Con);
+             Cmd.Parameters.Add("@Person_Email", System.Data.SqlDbType.NVarChar);
+                 Cmd.Parameters.Add("@Person_Password", System.Data.SqlDbType.NVarChar);
+                 Cmd.Parameters.Add("@Person_First_Name", System.Data.SqlDbType.NVarChar);
+                 Cmd.Parameters.Add("@Person_Last_Name", System.Data.SqlDbType.NVarChar);
+             Cmd.Parameters.Add("@Person_EGN", System.Data.SqlDbType.NVarChar);
+             
             try
             {
                 string newPassword = Password.GeneratePassword();
                 string newEGN = EGN.EGNMaker(gender);
                 string newEmail = Email.EmailMaker(First_Name, Last_Name, newEGN);
                 Cmd.Parameters["@Person_Password"].Value = newPassword;
-                Cmd.Parameters["@Person_First_Name"].Value = First_Name;
-                Cmd.Parameters["@Person_Last_Name"].Value = Last_Name;
-                Cmd.Parameters["@Person_EGN"].Value = newEGN;
-                Cmd.Parameters["@Person_Email"].Value = newEmail;
-            
-                Con.Open();
-                Cmd.ExecuteNonQuery();  
-                Console.WriteLine($"('{newEmail}', '{newPassword}', '{First_Name}', '{Last_Name}', {newEGN})");
+                 Cmd.Parameters["@Person_First_Name"].Value = First_Name;
+                 Cmd.Parameters["@Person_Last_Name"].Value = Last_Name;
+                 Cmd.Parameters["@Person_EGN"].Value = newEGN;
+                 Cmd.Parameters["@Person_Email"].Value = newEmail;
+
+                 Con.Open();
+                 Cmd.ExecuteNonQuery();
+                 index = (int)CmdId.ExecuteScalar();
+                Console.WriteLine($"('{newEmail}', '{newPassword}', N'{First_Name}', N'{Last_Name}', {newEGN}),");
             }
             catch (SqlException e)
             {
                 Console.WriteLine("Error Generated. Details: " + e.ToString());
             }
-
         }
+        public int Index { get { return index; } }
     }
 }
