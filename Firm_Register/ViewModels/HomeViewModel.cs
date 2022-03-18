7ef1ef82562665401095485a00c74ad9747a7a5f
Alex Dimitrov <78638967+AlexD174@users.noreply.github.com>
2022-03-19 00:38:56 +0200

    Add files via upload

---
+++ Firm_Register/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Firm_Register.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Firm_Register.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<People> People { get; set; }
+        public IEnumerable<Firms> Firms { get; set; }
+        public IEnumerable<Regions> Regions { get; set; }
+    }
+}
