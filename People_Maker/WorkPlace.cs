bfc6e6b79d4b84cb2092ea2b592d1eed0e1a1a99
Alex Dimitrov <78638967+AlexD174@users.noreply.github.com>
2022-03-18 18:01:57 +0200

    Update WorkPlace.cs

--- People_Maker/WorkPlace.cs
+++ People_Maker/WorkPlace.cs
@@ -1,12 +1,41 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace People_Maker
 {
     class WorkPlace
     {
+        public string toDataBase = "";
+        public WorkPlace(int person_Id, int firm_Id, int role_Id)
+        {
+            string conString = "Data Source = (LocalDb)\\MSSQLLocalDB; Initial Catalog = Firm_Register; Integrated Security=True";
+            SqlConnection Con = new SqlConnection(@conString);
+            SqlCommand Cmd = new SqlCommand("INSERT INTO WorkPlaces" +
+            "(Person_Id, Firm_Id, Role_Id) " +
+                    "VALUES(@Person_Id, @Firm_Id, @Role_Id)", Con);
+            Cmd.Parameters.Add("@Person_Id", System.Data.SqlDbType.Int);
+            Cmd.Parameters.Add("@Firm_Id", System.Data.SqlDbType.Int);
+            Cmd.Parameters.Add("@Role_Id", System.Data.SqlDbType.Int);
+
+            try
+            {
+                Cmd.Parameters["@Person_Id"].Value = person_Id;
+                Cmd.Parameters["@Firm_Id"].Value = firm_Id;
+                Cmd.Parameters["@Role_Id"].Value = role_Id;
+
+                Con.Open();
+                Cmd.ExecuteNonQuery();
+                toDataBase = $"('{person_Id}', '{firm_Id}', '{role_Id}'),";
+                Con.Close();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+        }
     }
 }
