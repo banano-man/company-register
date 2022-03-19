c6ac4c0e2cc262c0d8fafdf5206b337f95c2b07b
Alex Dimitrov <78638967+AlexD174@users.noreply.github.com>
2022-03-19 08:45:51 +0200

    Update FirmViewModel.cs

--- Firm_Register/ViewModels/FirmViewModel.cs
+++ Firm_Register/ViewModels/FirmViewModel.cs
@@ -11,5 +11,6 @@ namespace Firm_Register.ViewModels
         public IEnumerable<Firms> Firms { get; set; }
         public IEnumerable<Regions> Regions { get; set; }
         public IEnumerable<WorkPlace> WorkPlaces { get; set; }
+        public IEnumerable<People> People { get; set; }
     }
 }
