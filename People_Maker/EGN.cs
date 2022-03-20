2856968122f279308f02ae91e98f08cee849ac21
Alex Dimitrov <78638967+AlexD174@users.noreply.github.com>
2022-03-20 07:36:39 +0200

    Update EGN.cs

--- People_Maker/EGN.cs
+++ People_Maker/EGN.cs
@@ -8,9 +8,11 @@ namespace People_Maker
 {
     public static class EGN
     {
+        //Клас за генериране на случайно ЕГН
         public static string EGNMaker(char gender)
         {
             var egn = new StringBuilder();
+            //Първи 6 цифри: година, месец, ден
             DateTime birth = new DateTime(1970, 1, 1);
             Random rnd = new Random();
             birth = birth.AddDays(rnd.Next(10000));
@@ -19,6 +21,7 @@ namespace People_Maker
             else egn.Append(birth.Month.ToString());
             if (birth.Day < 10) { egn.Append(0); egn.Append(birth.Day.ToString()); }
             else egn.Append(birth.Day.ToString());
+            //3-цифрено число за област и четност/нечетност за пол
             int r = 0;
             if (gender == 'm')
                 while (r % 2 != 1)
@@ -32,6 +35,7 @@ namespace People_Maker
             if (r < 10) egn.Append("00");
             else if (r < 100) egn.Append("0");
             egn.Append(r);
+            //контролна цифра образуваща се по долуизползваната формула
             int contr = 0;
             contr = egn[0] * 2 + egn[1] * 4 + egn[2] * 8 + egn[3] * 5 + egn[4] * 10 + egn[5] * 9 + egn[6] * 7 + egn[7] * 3 + egn[8] * 6;
             if (contr % 11 == 10) egn.Append(0);
